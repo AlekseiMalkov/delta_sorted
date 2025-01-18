105b0a570c320f0fe3df0e1024977dc696490893
Aleksei Malkov <aleksei.malkov@gmail.com>
2025-01-18 14:26:18 -0500

    simple build

--- spark/src/main/scala/org/apache/spark/sql/delta/skipping/MultiDimClustering.scala
+++ spark/src/main/scala/org/apache/spark/sql/delta/skipping/MultiDimClustering.scala
@@ -74,7 +74,6 @@ trait SpaceFillingCurveClustering extends MultiDimClustering {
     val conf = df.sparkSession.sessionState.conf
     val numRanges = conf.getConf(DeltaSQLConf.MDC_NUM_RANGE_IDS)
     val addNoise = conf.getConf(DeltaSQLConf.MDC_ADD_NOISE)
-    val sort = conf.getConf(DeltaSQLConf.MDC_SORT_WITHIN_PARTITIONS)
 
     val cols = colNames.map(df(_))
     val mdcCol = getClusteringExpression(cols, numRanges)
@@ -91,14 +90,7 @@ trait SpaceFillingCurveClustering extends MultiDimClustering {
         .repartitionByRange(approxNumPartitions, col(repartitionKeyColName))
     }
 
-    val optionallySortedDf = if (sort) {
-      repartitionedDf.sortWithinPartitions(repartitionKeyColName)
-    }
-    else {
-      repartitionedDf
-    }
-
-    optionallySortedDf.drop(repartitionKeyColName)
+    repartitionedDf.drop(repartitionKeyColName)
   }
 }
 
